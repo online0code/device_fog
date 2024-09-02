472c6f85ac4af8a99baeb50787172e5a2a43af3f
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-09-02 21:24:13 +0200

    fog: Append `_vendor` to conflicting vendor libs
    Change-Id: I74306c87e12079204223caccc0c77eacd4e01ec0
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,48 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
-# Initialize the helper for common
+function vendor_imports() {
+    cat <<EOF >>"$1"
+          "device/xiaomi/fog",
+          "hardware/google/interfaces",
+          "hardware/google/pixel",
+          "hardware/lineage/interfaces/power-libperfmgr",
+          "hardware/qcom-caf/bootctrl",
+          "hardware/qcom-caf/common/libqti-perfd-client",
+          "hardware/qcom-caf/sm8250",
+          "hardware/qcom-caf/wlan",
+          "hardware/xiaomi",
+          "vendor/qcom/opensource/commonsys/display",
+          "vendor/qcom/opensource/commonsys-intf/display",
+          "vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um",
+          "vendor/qcom/opensource/dataservices",
+          "vendor/qcom/opensource/display"
+EOF
+}
+
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+    case "$1" in
+            com.qualcomm.qti.dpm.api@1.0 | \
+            libmmosal | \
+            vendor.qti.hardware.fm@1.0 | \
+            vendor.qti.imsrtpservice@3.0)
+            echo "${1}_vendor"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+function lib_to_package_fixup() {
+        lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
+# Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
 # Copyright headers and guards
