aa6d3f2960d0826c622eb3d1162f80372a7dedd1
alternoegraha <noegrahachan@gmail.com>
2025-01-30 04:43:29 +0000

    Revert "fog: Bring back QTI wifidisplay"
    Switch to AOSP WFD
    
    This reverts commit defc5cae9c199db926044af8ad44eb6f8699c60c.

--- device.mk
+++ device.mk
@@ -539,5 +539,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
+# WiFi firmware symlinks
+PRODUCT_PACKAGES += \
+    firmware_wlan_mac.bin_symlink \
+    firmware_WCNSS_qcom_cfg.ini_symlink
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/fog/fog-vendor.mk)
